74116948e1de74368f919f2277f654cd5263a0e2
Nik Uzair <nikuzairsc@gmail.com>
2023-07-11 12:32:06 +0800

    Connecting multiple providers with each other (Dependent Providers)
    - Connecting dependent providers to filter provider

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -4,7 +4,6 @@ import 'package:meal_app/screens/categories.dart';
 import 'package:meal_app/screens/filters.dart';
 import 'package:meal_app/screens/meals.dart';
 import 'package:meal_app/widgets/main_drawer.dart';
-import 'package:meal_app/providers/meals_provider.dart';
 import 'package:meal_app/providers/favorites_provider.dart';
 import 'package:meal_app/providers/filters_provider.dart';
 
@@ -41,25 +40,8 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
 
-    final activeFilters = ref.watch(filtersProvider);
-
-    final availableMeals = meals.where((meal) {
-      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-        return false;
-      }
-      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-        return false;
-      }
-      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-        return false;
-      }
-      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-        return false;
-      }
-      return true;
-    }).toList();
+    final availableMeals = ref.watch(filteredMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
