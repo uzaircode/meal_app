67f400e678f8322c70c6b5efc90c3a7b52798578
Nik Uzair <nikuzairsc@gmail.com>
2023-07-11 12:32:46 +0800

    Update Icon logo based on the boolean status
    - Use tertiary operator to update the icon.

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -13,6 +13,9 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(title: Text(meal.title), actions: [
         IconButton(
@@ -28,7 +31,7 @@ class MealDetailsScreen extends ConsumerWidget {
               ),
             );
           },
-          icon: const Icon(Icons.star),
+          icon: Icon(isFavorite ? Icons.star : Icons.star_border_outlined),
         )
       ]),
       body: SingleChildScrollView(
